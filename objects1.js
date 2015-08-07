fb157909d51791e07c6e0eac2860fad64965d8e4
Stephen Young <stephenjayyoung@gmail.com>
2015-08-07 07:32:51 -0700

    added some comments to objects1.js

--- objects1.js
+++ objects1.js
@@ -1,5 +1,22 @@
+
+//two ways to make objects:
+//way 1:
+var me = {
+    name: "Steve",
+    age: 28
+};
+
+
+
+//way 2 (using the Object constructor)
 var me = {}
 var me = new Object()
 
 me["name"] = "Steve";
 me.age = 28;
+
+//overall, this section of Codecademy is somewhat basic. 
+//It covers using dot notation, creating objects, using object
+//constructors, and using arrays within objects, but it doesn't
+//delve very deep into any of these concepts. Decent intro to 
+//objects, though, and a good way to get your feet wet.
