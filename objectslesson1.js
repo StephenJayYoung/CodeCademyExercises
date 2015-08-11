2370310fdbef46f46b84fb01e97c2730ca04364c
Stephen Young <stephenjayyoung@gmail.com>
2015-08-11 06:51:07 -0700

    added a few more exercises

--- objectslesson1.js
+++ objectslesson1.js
@@ -76,3 +76,28 @@ susan.setAge = setAge;
 // here, update Susan's age to 35 using the method
 susan.setAge(35);
 
+//////////////////////////////////////////////////
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
+
+////////////////////////////////////////////
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(age, color) {
+    this.age = age;
+    this.color = color;
+    
+    }
+
