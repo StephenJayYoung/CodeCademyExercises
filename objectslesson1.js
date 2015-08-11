166fe32ccae373e36303d4409dfcf17fb4110041
Stephen Young <stephenjayyoung@gmail.com>
2015-08-10 18:09:24 -0700

    added some more exercises from objects lesson

--- objectslesson1.js
+++ objectslesson1.js
@@ -12,6 +12,7 @@ var age1 = bob.age;
 // finish this code by saving Susan's information
 var name2 = susan.name;
 var age2 = susan.age;
+
 ////////////////////////////
 // Our bob object again, but made using a constructor this time 
 var bob = new Object();
@@ -26,3 +27,52 @@ var susan1 = {
 var susan2 = new Object();
 susan2.name = "Susan Jordan";
 susan2.age = 24;
+
+////////////////////////////////
+
+var bob = new Object();
+bob.age = 17;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+
+bob.getYearOfBirth = function () {
+  return 2014 - bob.age;
+};
+console.log(bob.getYearOfBirth());
+
+////////////////////////////////////
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+// and down here we just use the method we already made
+bob.setAge = setAge;
+
+// change bob's age to 50 here
+bob.setAge(50);
+
+/////////////////////////////////////
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+  
+// make susan here, and first give her an age of 25
+var susan = new Object();
+susan.age = 25;
+susan.setAge = setAge;
+
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35);
+
